817a63a5c8924555c71d8806ace8a9946f78e38b
Nyanjuimarvin <marnyanjui@gmail.com>
2022-02-17 21:13:48 +0300

    Finished RGR for Guessed words List

--- src/main/java/models/Hangman.java
+++ src/main/java/models/Hangman.java
@@ -1,15 +1,19 @@
 package models;
 
-import java.lang.reflect.Array;
-import java.util.Random;
+import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Hangman {
 
     private char guessChar;
-    private String[] wordsToGuess = {"waldo","berzerk","mongolia"};
+    private final String[] wordsToGuess = {"waldo","berzerk","mongolia"};
 
+    //Random number < 1 - 3>
     int currentIndex = ThreadLocalRandom.current().nextInt(wordsToGuess.length);
+
+    //No of guesses == Chars in string
+    long guessCount = wordsToGuess[currentIndex].chars().filter( ch -> ch != ' ').count();
+    ArrayList<Character> everyGuess = new ArrayList<>();
 
     public void setGuessChar(char guessChar) {
         this.guessChar = guessChar;
@@ -19,9 +23,17 @@ public class Hangman {
         return guessChar;
     }
 
+    public String currentGuessWord(){
+        return wordsToGuess[currentIndex];
+    }
+
     public String wordToGuessAsCharacters(){
         String hiddenWord = wordsToGuess[currentIndex].replaceAll(".","_");
         return hiddenWord;
+    }
+
+    public void guessedWords ( ){
+        everyGuess.add(getGuessChar());
     }
 
 
--- src/test/java/models/HangmanTest.java
+++ src/test/java/models/HangmanTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HangmanTest {
@@ -16,26 +19,40 @@ class HangmanTest {
 
     @Test
     @DisplayName("Class Is Instantiated")
-    public void runHangMan_isObjectIntantaited(){
+    public void HangMan_isObjectIntantaited(){
 
         assertTrue(testHangman instanceof Hangman);
     }
 
     @Test
     @DisplayName("Random Integer Range")
-    void runHangMan_checkRandomRange_Int() {
+    void threadLocalRandom_checkRandomRange_Int() {
         assertEquals(testHangman.currentIndex,testHangman.currentIndex);
     }
 
     @Test
     @DisplayName("Should Check If The Character Is Set")
-    void runHangMan_checkGetterAndSetter() {
+    void getGuessChar_checkGetterAndSetter() {
         testHangman.setGuessChar('w');
         assertEquals('w',testHangman.getGuessChar());
     }
 
     @Test
-    void runHangman_areWordsHiddenAsCharacters_String() {
+    void wordsToGuessAsCharacters_areWordsHiddenAsCharacters_String() {
         assertEquals(testHangman.wordToGuessAsCharacters(),testHangman.wordToGuessAsCharacters());
+    }
+
+    @Test
+    @DisplayName("Should Return List Of Guessed Characters")
+    void guessedWords_areGuessesStored_ArrayList() {
+        List<Character> testGuesses = new ArrayList<>();
+        testHangman.setGuessChar('e');
+        testGuesses.add(testHangman.getGuessChar());
+        testHangman.guessedWords();
+        testHangman.setGuessChar('g');
+        testGuesses.add(testHangman.getGuessChar());
+        testHangman.guessedWords();
+
+        assertEquals(testGuesses,testHangman.everyGuess);
     }
 }
